ab624fdbee5e3868b701e591fac7a8c6e30b002c
V.G. Bulavintsev <golem.md@gmail.com>
2025-11-28 17:22:29 +0100

    Fix dep

--- api/apps/meriter/src/api-v1/invites/invites.module.ts
+++ api/apps/meriter/src/api-v1/invites/invites.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DomainModule } from '../../domain.module';
 import { InvitesController } from './invites.controller';
+import { User, UserSchema } from '../../domain/models/user/user.schema';
 
 @Module({
-  imports: [DomainModule],
+  imports: [
+    DomainModule,
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [InvitesController],
 })
 export class InvitesModule {}
